a78c6097d3bf274c59e5d02312abcbb438a964e4
Gaspar Nagy <gaspar.nagy@gmail.com>
2017-01-26 16:17:12 +0100

    refactor ScenarioTestCase

--- xUnitPlay/TestArtifacts/ScenarioTestCase.cs
+++ xUnitPlay/TestArtifacts/ScenarioTestCase.cs
@@ -11,42 +11,22 @@ using Xunit.Sdk;
 
 namespace xUnitPlay.TestArtifacts
 {
-    [Serializable]
-    public class ScenarioTestCase : LongLivedMarshalByRefObject, ITestCase, ITestMethod, IMethodInfo, IXunitTestCase, IReflectionMethodInfo
+    public class ScenarioTestCase : LongLivedMarshalByRefObject, ITestMethod, IXunitTestCase, IReflectionMethodInfo
     {
-        public ITestClass TestClass { get; set; }
-        public string Name { get; set; }
+        public FeatureFileTestClass FeatureFile { get; private set; }
+        public string Name { get; private set; }
 
-        public FeatureFileTestClass FeatureFile => (FeatureFileTestClass)TestClass;
         public string DisplayName => Name;
         public string UniqueID => $"{FeatureFile.RelativePath};{Name}";
 
-        public ITestMethod TestMethod => this;
-
-        public void Deserialize(IXunitSerializationInfo data)
-        {
-            TestClass = data.GetValue<FeatureFileTestClass>("TestClass");
-            Name = data.GetValue<string>("Name");
-        }
-
-        public void Serialize(IXunitSerializationInfo data)
-        {
-            data.AddValue("TestClass", TestClass);
-            data.AddValue("Name", Name);
-        }
-
-        public string SkipReason { get; set; }
         public ISourceInformation SourceInformation { get; set; }
-        public object[] TestMethodArguments { get; set; }
+        public string SkipReason { get; set; }
         public Dictionary<string, List<string>> Traits { get; set; }
+        object[] ITestCase.TestMethodArguments => null;
 
-        public bool IsAbstract { get; set; }
-        public bool IsGenericMethodDefinition { get; set; }
-        public bool IsPublic { get { return true; } }
-        public bool IsStatic { get; set; }
-        public ITypeInfo ReturnType { get; set; }
-        public MethodInfo MethodInfo { get { throw new NotImplementedException("ScenarioTestCase.MethodInfo"); } }
-        public ITypeInfo Type => TestClass.Class;
+        public ITestClass TestClass => FeatureFile;
+        public ITestMethod TestMethod => this;
+        public IMethodInfo Method => this;
 
         public ScenarioTestCase()
         {
@@ -55,13 +35,24 @@ namespace xUnitPlay.TestArtifacts
 
         public ScenarioTestCase(FeatureFileTestClass featureFileTestClass, Scenario scenario, string[] featureTags) : this()
         {
-            TestClass = featureFileTestClass;
+            FeatureFile = featureFileTestClass;
             Name = scenario.Name;
             SourceInformation = new SourceInformation { FileName = featureFileTestClass.FeatureFilePath, LineNumber = scenario.Location?.Line };
             Traits.Add("Category", featureTags.Concat(scenario.Tags.GetTags()).ToList());
         }
 
-        /// <inheritdoc/>
+        public void Deserialize(IXunitSerializationInfo data)
+        {
+            FeatureFile = data.GetValue<FeatureFileTestClass>("FeatureFile");
+            Name = data.GetValue<string>("Name");
+        }
+
+        public void Serialize(IXunitSerializationInfo data)
+        {
+            data.AddValue("FeatureFile", FeatureFile);
+            data.AddValue("Name", Name);
+        }
+
         public virtual Task<RunSummary> RunAsync(IMessageSink diagnosticMessageSink,
                                                  IMessageBus messageBus,
                                                  object[] constructorArguments,
@@ -69,26 +60,21 @@ namespace xUnitPlay.TestArtifacts
                                                  CancellationTokenSource cancellationTokenSource)
             => new ScenarioTestCaseRunner(this, messageBus, aggregator, cancellationTokenSource).RunAsync();
 
-        public IMethodInfo Method { get { return this; } }
-        public IEnumerable<IAttributeInfo> GetCustomAttributes(string assemblyQualifiedAttributeTypeName)
+        #region IMethodInfo default implementation
+        bool IMethodInfo.IsAbstract => false;
+        bool IMethodInfo.IsGenericMethodDefinition => false;
+        bool IMethodInfo.IsPublic => true;
+        bool IMethodInfo.IsStatic => false;
+        ITypeInfo IMethodInfo.ReturnType => null;
+        ITypeInfo IMethodInfo.Type => TestClass.Class;
+        IEnumerable<IAttributeInfo> IMethodInfo.GetCustomAttributes(string assemblyQualifiedAttributeTypeName) => Enumerable.Empty<IAttributeInfo>();
+        IEnumerable<ITypeInfo> IMethodInfo.GetGenericArguments() => Enumerable.Empty<ITypeInfo>();
+        IEnumerable<IParameterInfo> IMethodInfo.GetParameters() => Enumerable.Empty<IParameterInfo>();
+        IMethodInfo IMethodInfo.MakeGenericMethod(params ITypeInfo[] typeArguments)
         {
-            throw new NotImplementedException("GetCustomAttributes.GetCustomAttributes");
+            throw new NotSupportedException("ScenarioTestCase.MakeGenericMethod");
         }
-
-        public IEnumerable<ITypeInfo> GetGenericArguments()
-        {
-            throw new NotImplementedException("ScenarioTestCase.GetGenericArguments");
-        }
-
-        public IEnumerable<IParameterInfo> GetParameters()
-        {
-            throw new NotImplementedException("ScenarioTestCase.GetParameters");
-        }
-
-        public IMethodInfo MakeGenericMethod(params ITypeInfo[] typeArguments)
-        {
-            throw new NotImplementedException("ScenarioTestCase.MakeGenericMethod");
-        }
-
+        MethodInfo IReflectionMethodInfo.MethodInfo { get { throw new NotSupportedException("MethodInfo is not available for scenarios"); } }
+        #endregion
     }
 }
