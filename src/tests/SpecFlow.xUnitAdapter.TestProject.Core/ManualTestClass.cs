3a446d6e3ee05b749c903e7105526e8821cb64e7
Gaspar Nagy <gaspar.nagy@gmail.com>
2017-04-20 13:36:31 +0200

    .net core test project

---
+++ src/tests/SpecFlow.xUnitAdapter.TestProject.Core/ManualTestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace SpecFlow.xUnitAdapter.TestProject.Core
+{
+    public class ManualTestClass
+    {
+        [Fact]
+        public void TestThis()
+        {
+
+        }
+    }
+}
